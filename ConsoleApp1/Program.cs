a059bc51225d7b5dafc6a39f0952c36e5179e262
Kevin <kevnaf@gmail.com>
2018-03-20 21:36:11 -0700

    Calc fist pass

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Intro();
+        }
+
+        public static void Intro()
+        {
+            Console.WriteLine("Welcome to this roudamentry calculator");
+            Console.WriteLine("Lets pick 2 numbers to play with.\nPlease enter the first number");
+            double userNumberOne = Double.Parse(Console.ReadLine());
+            Console.WriteLine("and now the second:");
+            double userNumberTwo = Double.Parse(Console.ReadLine());
+            Console.WriteLine("You can:\na) Add\nb) Multiply\nc) Divide\nd) Subtract");
+            Console.WriteLine("please enter your choice");
+            string userPath = Console.ReadLine();
+            if (userPath == "a")
+            {
+                Console.WriteLine(Add(userNumberOne, userNumberTwo));
+            }
+            if (userPath == "b")
+            {
+                Console.WriteLine(Multiply(userNumberOne, userNumberTwo));
+            }
+            if (userPath == "c")
+            {
+                Console.WriteLine(Divide(userNumberOne, userNumberTwo));
+            }
+            if (userPath == "d")
+            {
+                Console.WriteLine(Subtract(userNumberOne, userNumberTwo));
+            }
+            Console.ReadLine();
+        }
+
+        public static double Add (double doubleOne, double doubletwo)
+        {
+            return doubleOne + doubletwo;
+        }
+
+        public static double Multiply(double doubleOne, double doubletwo)
+        {
+            return doubleOne * doubletwo;
+        }
+
+        public static double Divide(double doubleOne, double doubletwo)
+        {
+            return doubleOne / doubletwo;
+        }
+
+        public static double Subtract(double doubleOne, double doubletwo)
+        {
+            return doubleOne - doubletwo;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # CalcApp
+
+A small test app intended to be a first foray into C#
+
+## Getting Started
+
+This is a small test app using visual studio 2017. You will need to install and run VS2017 in order to run and use the app.
+
+## Features
+
+You can enter two numbers that will be minipulated in one of four ways. Add, Multiply, Divide, and Subtract.
+
+## Resources and Links
+
+I leveraged Stack overflow and the 7.0 in a nutshell publication. 
+
+## Licensing
+
+The code in this project is licensed under MIT license.
